51174dc832f2e6a7cb75f47aec665b7eec137c2c
Duc Nguyen <nguyenhoangminhduc@gmail.com>
2022-12-30 23:05:45 +0200

    Test deploy on PR

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nhminhduc/genshin_banner.git main:gh-pages
+git push -f https://${GITHUB_TOKEN}@github.com/nhminhduc/genshin_banner.git main:gh-pages
 
 cd -
