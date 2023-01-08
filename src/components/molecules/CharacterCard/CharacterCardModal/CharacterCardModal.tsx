36ecc4f1eba09778f9ff3c9beed7cf6a35268659
nhminhduc <nguyenhoangminhduc@gmail.com>
2023-01-08 23:45:58 +0200

    Run prettier

--- src/components/molecules/CharacterCard/CharacterCardModal/CharacterCardModal.tsx
+++ src/components/molecules/CharacterCard/CharacterCardModal/CharacterCardModal.tsx
@@ -38,22 +38,22 @@ const CharacterCardModal = ({
 
   return expanded
     ? ReactDOM.createPortal(
-      <FocusLock>
-        <div className="modal-backdrop fixed w-full h-full top-0 2xl:hidden z-20 flex justify-center top-20">
-          <div
-            aria-labelledby="modalTitle"
-            aria-modal={true}
-            className="h-min w-3/4 md:w-3/5 "
-            ref={modalRef}
-            role="dialog"
-            tabIndex={-1}
-          >
-            <CharacterCard character={character} className={className} />
+        <FocusLock>
+          <div className="modal-backdrop fixed w-full h-full top-0 2xl:hidden z-20 flex justify-center top-20">
+            <div
+              aria-labelledby="modalTitle"
+              aria-modal={true}
+              className="h-min w-3/4 md:w-3/5 "
+              ref={modalRef}
+              role="dialog"
+              tabIndex={-1}
+            >
+              <CharacterCard character={character} className={className} />
+            </div>
           </div>
-        </div>
-      </FocusLock>,
-      document.body,
-    )
+        </FocusLock>,
+        document.body,
+      )
     : null;
 };
 
