060beda6a2618086f8ad39b23988a718a9f74fac
nhminhduc <nguyenhoangminhduc@gmail.com>
2023-01-08 23:42:48 +0200

    Focus on modal on open mobile

--- src/components/molecules/CharacterCard/CharacterCardModal/CharacterCardModal.tsx
+++ src/components/molecules/CharacterCard/CharacterCardModal/CharacterCardModal.tsx
@@ -2,7 +2,7 @@ import CharacterCard, {
   CharacterCardProps,
 } from "components/molecules/CharacterCard/CharacterCard";
 import useModal from "hooks/useModal";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import FocusLock from "react-focus-lock";
 
@@ -32,24 +32,28 @@ const CharacterCardModal = ({
     setExpanded(false);
   });
 
+  useEffect(() => {
+    modalRef.current?.focus();
+  }, []);
+
   return expanded
     ? ReactDOM.createPortal(
-        <FocusLock>
-          <div className="modal-backdrop fixed w-full h-full top-0 2xl:hidden z-20 flex justify-center top-20">
-            <div
-              aria-labelledby="modalTitle"
-              aria-modal={true}
-              className="h-min w-3/4 md:w-3/5 "
-              ref={modalRef}
-              role="dialog"
-              tabIndex={-1}
-            >
-              <CharacterCard character={character} className={className} />
-            </div>
+      <FocusLock>
+        <div className="modal-backdrop fixed w-full h-full top-0 2xl:hidden z-20 flex justify-center top-20">
+          <div
+            aria-labelledby="modalTitle"
+            aria-modal={true}
+            className="h-min w-3/4 md:w-3/5 "
+            ref={modalRef}
+            role="dialog"
+            tabIndex={-1}
+          >
+            <CharacterCard character={character} className={className} />
           </div>
-        </FocusLock>,
-        document.body,
-      )
+        </div>
+      </FocusLock>,
+      document.body,
+    )
     : null;
 };
 
